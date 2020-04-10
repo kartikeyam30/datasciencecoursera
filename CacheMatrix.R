387be4bfda761091fea7ab9908a27a060d8f7585
pynoob11 <kartikey.mehrotra12@gmail.com>
2020-04-10 15:51:46 +0530

    Week2Pt1

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -21,19 +21,3 @@ makeCacheMatrix <- function( m = matrix() ) {
        getInverse = getInverse)
 }
 
-
-cacheSolve <- function(x, ...) {
-  m <- x$getInverse()
-  if( !is.null(m) ) {
-    message("getting cached data")
-    return(m)
-  }
-  
-  data <- x$get()
-  
-  m <- solve(data) %*% data
-  
-  x$setInverse(m)
-  
-  m
-}
